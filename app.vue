0c9617546f7af6763e3df205a4f6e8d4c8a10657
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-05-22 19:40:23 +0300

    add locale switching

--- app.vue
+++ app.vue
@@ -10,12 +10,31 @@ const sidebar = [
   'sidebar.finance',
   'sidebar.messages',
 ]
+
+const { locale, locales } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
+
+const currentLocale = computed(() => locales.value.find(l => locale.value === l.code))
+const availableLocales = computed(() => {
+  return (locales.value).filter(i => i.code !== locale.value).map(i => ({
+    text: i.name,
+    href: switchLocalePath(i.code),
+  }))
+})
 </script>
 
 <template>
-  <ul>
-    <li class="capitalize" v-for="i in sidebar" :key="i">
-      {{ $t(i) }}
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li v-for="locale in locales" :key="locale.code">
+        <a :href="locale.code" class="capitalize">{{ locale.name }}</a>
+      </li>
+    </ul>
+
+    <ul>
+      <li class="capitalize" v-for="i in sidebar" :key="i">
+        {{ $t(i) }}
+      </li>
+    </ul>
+  </div>
 </template>
