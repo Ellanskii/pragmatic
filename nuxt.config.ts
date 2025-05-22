2274eb4db016bd99d8fbd5a4350b018d3c24de77
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-05-22 15:38:35 +0300

    add i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
-})
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/i18n'],
+
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+      },
+      {
+        code: 'ru',
+        name: 'Русский',
+      },
+    ],
+  },
+});
