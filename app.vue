b366606a58fb041d133476b9f7d49bb0c0d0bf5c
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-05-26 10:21:21 +0300

    WIP localeSelect

--- app.vue
+++ app.vue
@@ -10,27 +10,11 @@ const sidebar = [
   'sidebar.finance',
   'sidebar.messages',
 ]
-
-const { locale, locales } = useI18n()
-const switchLocalePath = useSwitchLocalePath()
-
-const currentLocale = computed(() => locales.value.find(l => locale.value === l.code))
-const availableLocales = computed(() => {
-  return (locales.value).filter(i => i.code !== locale.value).map(i => ({
-    text: i.name,
-    href: switchLocalePath(i.code),
-  }))
-})
 </script>
 
 <template>
   <div>
-    <ul>
-      <li v-for="locale in locales" :key="locale.code">
-        <a :href="locale.code" class="capitalize">{{ locale.name }}</a>
-      </li>
-    </ul>
-
+    <LocaleSelect />
     <ul>
       <li class="capitalize" v-for="i in sidebar" :key="i">
         {{ $t(i) }}
