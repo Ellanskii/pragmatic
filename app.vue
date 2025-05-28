a9bda022d871420651516fedff1430632f1c3659
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-05-28 15:40:57 +0300

    fix typo

--- app.vue
+++ app.vue
@@ -3,15 +3,17 @@
 
 <template>
   <UApp>
-    <header clas="bg-default/75 backdrop-blur border-b border-default h-(--ui-header-height) sticky top-0 z-50">
-      <div class="w-full max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between gap-3 h-full">
+    <header class="border-b-1 border-default sticky top-0 z-50">
+      <div class="w-full max-w-(--ui-container) mx-auto py-2 px-4 sm:px-6 lg:px-8 flex items-center justify-between gap-3 h-full">
         <div class="flex items-center justify-end lg:flex-1 gap-1.5">
           <LocaleSelect />
           <ThemeSwitcher />
         </div>
       </div>
     </header>
-    <AppSidebar />
-    <NuxtPage />
+    <div class="flex">
+      <AppSidebar class="pr-2 border-r-1 border-default" />
+      <NuxtPage />
+    </div>
   </UApp>
 </template>
