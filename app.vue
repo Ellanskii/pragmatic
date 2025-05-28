142ae9b170fefdce9db42a6aef61f0c25cfa92af
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-05-28 16:24:53 +0300

    fix layout

--- app.vue
+++ app.vue
@@ -4,7 +4,8 @@
 <template>
   <UApp>
     <header class="bg-default/75 backdrop-blur border-b-1 border-default sticky top-0 z-50">
-      <div class="w-full max-w-(--ui-container) mx-auto py-2 px-4 sm:px-6 lg:px-8 flex items-center justify-between gap-3 h-full">
+      <div class="w-full mx-auto py-2 px-4 sm:px-6 lg:px-8 flex items-center justify-between gap-3 h-full">
+        <h1 class="font-black uppercase">Pragmatic</h1>
         <div class="flex items-center justify-end lg:flex-1 gap-1.5">
           <LocaleSelect />
           <ThemeSwitcher />
